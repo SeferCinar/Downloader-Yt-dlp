fe189d05ac626a352cf3e24b033b6a57f24690ac
Sefer Çınar <50600541+SeferCinar@users.noreply.github.com>
2024-01-20 23:54:36 +0300

    Some Improvment

--- downloadGUI.py
+++ downloadGUI.py
@@ -2,38 +2,56 @@ import tkinter as tk
 import random
 import subprocess
 window = tk.Tk()
-window.geometry("300x150")
+window.geometry("500x200")
 window.title("Downloader App")
-var = tk.StringVar(value=" ")
+var = tk.StringVar(value="Video")
+labelText = tk.StringVar(value="Please select")
+statusText = tk.StringVar(value="")
+linkText= tk.StringVar(value="")
 selectionText = tk.Label(
     text="Please select",
-    width=30
-    )
+    width=30,
+    font=("sans",16),
+    textvariable=labelText,
+    ).grid(row=0,column=0)
+staticmethod = tk.Label(
+    textvariable=statusText,
+    font="sans"
+).grid(row=4,column=0)
 def getVideo():
-    url = link.get()
-    selection = var.get()
-    if selection == "Audio":
-        subprocess.run(["yt-dlp","-x",f"{url}","--restrict-filenames"])
+    if linkText.get() == "":
+        statusText.set(value="Please enter a link")
     else:
-        subprocess.run(["yt-dlp",f"{url}","--restrict-filenames"])
+        url = linkText.get()
+        selection = var.get()
+        if selection == "Audio":
+            subprocess.run(["yt-dlp","-x",f"{url}","--restrict-filenames"])
+        else:
+            subprocess.run(["yt-dlp",f"{url}","--restrict-filenames"])  
+    
     
 def selection():
-    selection = var.get()
-
+    selection = var.get() 
+    labelText.set(selection)
     
-    selectionText.config(text=selection)
 frame = tk.Frame()
-R1 = tk.Radiobutton( master=frame,text="Audio",
+R1 = tk.Radiobutton( master=window,text="Audio",
                     value="Audio",  
                     command=selection,
-                    variable=var)  
-R1.pack()  
+                    variable=var,
+                    border=5,
+                    font="sans",
+                    justify="left"
+                    ).grid(row=1,column=1)
+ 
   
-R2 = tk.Radiobutton( master=frame,text="Video",
+R2 = tk.Radiobutton( master=window,text="Video",
                     value="Video",  
                    command=selection,
-                   variable=var)  
-R2.pack( )     
+                   font="sans",
+                   justify="left",
+                   variable=var).grid(row=2,column=1)
+ 
 
 button = tk.Button(
     text="Download",
@@ -42,12 +60,14 @@ button = tk.Button(
     bg="gray",
     fg="black",
     activebackground="yellow",
-    command=getVideo
-    )
-link = tk.Entry()
-link.pack()
-frame.pack()
-selectionText.pack()
-button.pack()
+    highlightbackground="green",
+    command=getVideo,
+    font="sans",
+    ).grid(row=2,column=0)
+link = tk.Entry(
+    font=("Helvetica", 12),
+    textvariable=linkText
+).grid(row=1,column=0)
+
 
 window.mainloop()
